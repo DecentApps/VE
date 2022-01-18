d02c8885f993a3e23a8252b10a6a8003077047d6
ydra9 <ydra.nine@gmail.com>
2022-01-18 12:43:48 +0700

    constructor

--- contracts/IDO/IDO.sol
+++ contracts/IDO/IDO.sol
@@ -8,8 +8,25 @@ import "./IIDO.sol";
  * @dev Implementation of the {IDO} interface.
  */
 contract IDO is IIDO {
+    address private _owner; /* deployers' address*/
+    address private _token; /* BEP20 token address for the IDO */
+    address private _stablecoin; /* stablecoin address for the IDO. Used only for getting the rate between native coinand stablecoin */
+    address private _panecakePool; /* panecake pool address between stablecoin and native coin */
+    uint256 private _ratio; /* fixed ratio for the offering between stablcoin and native coin */
 
-    constructor() {}
+    constructor(
+        address __token,
+        address __stablecoin,
+        address __panecakePool,
+        uint256 __ratio
+    ) {
+        _owner = msg.sender;
+        _token = __token;
+        _stablecoin = __stablecoin;
+        _panecakePool = __panecakePool;
+        _ratio = __ratio;
+    }
+
     function getCurrentRate() external view override returns (uint256) {}
 
     function getFixedRate(address) external view override returns (uint256) {}
