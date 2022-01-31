eb0f5c9c49b582b0a8abbde2660565fa0149ba6e
ydra9 <ydra.nine@gmail.com>
2022-01-31 19:17:58 +0700

    tests for IDO

---
+++ test/IDO.js
@@ -0,0 +1,38 @@
+const { expect, assert } = require("chai");
+const { ethers } = require("hardhat");
+require("dotenv").config();
+
+describe("IDO for VEC smart contract", function () {
+  it("Checking IDO contract functionality", async function () {
+    const accounts = await hre.ethers.getSigners();
+    const ido = await ethers.getContractFactory("IDO");
+    const CONTRACT_ADDR = process.env.IDO_ADDR;
+
+    const vecIDO = await ido.attach("0x" + CONTRACT_ADDR);
+    const deployer = vecIDO.signer.address;
+    const usdtAddr = await vecIDO.getStablecoinAddress();
+    const vecAddr = await vecIDO.getTokenAddress();
+    const vecDecimals = 8;
+    const bnbDecimals = 18;
+    const initallyOffered = await vecIDO.getInitialOffered();
+    const ratio = await vecIDO.getFixedRate();
+
+    console.log(`IDO address: ${CONTRACT_ADDR}`);
+    console.log(`Deployer is: ${deployer}`);
+    console.log(`VEC address: ${vecAddr}`);
+    console.log(`USDT address: ${usdtAddr}`);
+
+    console.log(`Initial offer: ${initallyOffered / 10 ** vecDecimals}`);
+    console.log(`Fixed ratio of VEC/USDT: ${ratio}`);
+
+    let coinsExchnged = await vecIDO.totalInNative();
+    let vecSent = await vecIDO.totalOut();
+
+    console.log(`BNB so far: ${coinsExchnged / 10 ** bnbDecimals} BNB`);
+    console.log(`VEC distributed so far: ${vecSent / 10 ** vecDecimals} VEC`);
+
+    let currentRate = await vecIDO.getCurrentRate(1 * 10 ** vecDecimals);
+
+    console.log(`Current rate : ${currentRate}`);
+  });
+});
