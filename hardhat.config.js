8944ee614fc8e45dd95c358b1442ad3fc2c75ba3
ydra9 <ydra.nine@gmail.com>
2022-02-03 18:38:37 +0700

    adding correct pancake factory and router addresses

--- hardhat.config.js
+++ hardhat.config.js
@@ -34,18 +34,28 @@ module.exports = {
     bscMainnet: {
       chainId: 56,
       url: "https://bsc-dataseed.binance.org/",
-      accounts: [`0x${process.env.M_ISSUER_KEY}`, `0x${process.env.M_TESTER_KEY}`],
+      accounts: [
+        `0x${process.env.M_ISSUER_KEY}`,
+        `0x${process.env.M_TESTER_KEY}`,
+      ],
       from: `0x${process.env.M_ISSUER_KEY}`,
       gas: 2100000,
       gasPrice: 8000000000,
+      swapFactory: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
+      swapRouter: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
     },
     bscTestnet: {
       chainId: 97,
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-      accounts: [`0x${process.env.T_ISSUER_KEY}`, `0x${process.env.T_TESTER_KEY}`],
+      accounts: [
+        `0x${process.env.T_ISSUER_KEY}`,
+        `0x${process.env.T_TESTER_KEY}`,
+      ],
       from: `0x${process.env.T_ISSUER_KEY}`,
       gas: 2100000,
       gasPrice: 10000000000,
+      swapFactory: "0x182859893230dC89b114d6e2D547BFFE30474a21",
+      swapRouter: "0xdc4904b5f716Ff30d8495e35dC99c109bb5eCf81",
     },
   },
 };
