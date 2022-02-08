269726b3939d0654795cd9c9a8d4f942537aa199
ydra9 <ydra.nine@gmail.com>
2022-02-08 12:32:02 +0700

    changing factory for testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -43,6 +43,8 @@ module.exports = {
       gasPrice: 8000000000,
       swapFactory: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
       swapRouter: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+      usdt: "0x55d398326f99059fF775485246999027B3197955",
+      pool: "0x16b9a82891338f9ba80e2d6970fdda79d1eb0dae",
     },
     bscTestnet: {
       chainId: 97,
@@ -54,8 +56,10 @@ module.exports = {
       from: `0x${process.env.T_ISSUER_KEY}`,
       gas: 2100000,
       gasPrice: 10000000000,
-      swapFactory: "0x182859893230dC89b114d6e2D547BFFE30474a21",
-      swapRouter: "0xdc4904b5f716Ff30d8495e35dC99c109bb5eCf81",
+      swapFactory: "0xb7926c0430afb07aa7defde6da862ae0bde767bc ",
+      swapRouter: "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3",
+      usdt: "0x7ef95a0fee0dd31b22626fa2e10ee6a223f8a684",
+      pool: "0xF855E52ecc8b3b795Ac289f85F6Fd7A99883492b",
     },
   },
 };
